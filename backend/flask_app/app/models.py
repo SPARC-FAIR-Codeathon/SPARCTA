2e2ea1061d98b53a2482264920c45f410c695f1d
codemeleon <anmol.kiran@gmail.com>
2024-08-12 15:32:40 +0100

    updated tables

--- backend/flask_app/app/models.py
+++ backend/flask_app/app/models.py
@@ -7,25 +7,24 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.database import Base
 
-
-class Image(Base):
-    __tablename__ = 'images'
-    id = Column(Integer, primary_key=True)
-    image_name = Column(String(255), unique=True, nullable=False)
-    image_url = Column(String(255), unique=True, nullable=False)
-    upload_date = Column(DateTime(timezone=True), server_default=func.now())
-    annotations: Mapped[List["Annotation"]] = relationship(
-        "Annotation", back_populates="image")
-
-    # annotations: Mapped[List["Annotation"]] = relationship(
-    #     back_populates="parent")
-
-    def __init__(self, name=None, url=None):
-        self.image_name = name
-        self.image_url = url
-
-    def __repr__(self):
-        return f'<Image {self.image_name!r}>'
+# class Image(Base):
+#     __tablename__ = 'images'
+#     id = Column(Integer, primary_key=True)
+#     image_name = Column(String(255), unique=True, nullable=False)
+#     image_url = Column(String(255), unique=True, nullable=False)
+#     upload_date = Column(DateTime(timezone=True), server_default=func.now())
+#     annotations: Mapped[List["Annotation"]] = relationship(
+#         "Annotation", back_populates="image")
+#
+#     # annotations: Mapped[List["Annotation"]] = relationship(
+#     #     back_populates="parent")
+#
+#     def __init__(self, name=None, url=None):
+#         self.image_name = name
+#         self.image_url = url
+#
+#     def __repr__(self):
+#         return f'<Image {self.image_name!r}>'
 
 
 class Annotation(Base):
@@ -33,21 +32,24 @@ class Annotation(Base):
     id = Column(Integer, primary_key=True)
     # image: Mapped["Image"] = relationship(back_populates="image")
     user_id = Column(Integer)
-    annotation = Column(String(1024), unique=False, nullable=False)
-    annotation_coordinates = Column(String(1024), unique=False,
-                                    nullable=False)  # JSON
+    image_id = Column(String(1024), unique=False, nullable=False)
+    annotation_id = Column(String(64), unique=True, nullable=False)
+    annotation = Column(String(1024 * 2), unique=False, nullable=False)
     annotation_created_date = Column(DateTime(timezone=True),
                                      server_default=func.now())
     annotation_updated_date = Column(DateTime(timezone=True),
                                      server_default=func.now(),
                                      onupdate=func.now())
-    image_id = Column(Integer, ForeignKey('images.id'))
-    image: Mapped["Image"] = relationship("Image",
-                                          back_populates="annotations")
 
-    def __init__(self, annotation=None, annotation_coordinates=None):
+    # image_id = Column(Integer, ForeignKey('images.id'))
+    # image: Mapped["Image"] = relationship("Image",
+    #                                       back_populates="annotations")
+    #
+    def __init__(self, user_id, image_id, annotation_id, annotation):
+        self.user_id = user_id
+        self.image_id = image_id
+        self.annotation_id = annotation_id
         self.annotation = annotation
-        self.annotation_coordinates = annotation_coordinates
 
     def __repr__(self):
         return f'<Annotation {self.annotation!r}>'
