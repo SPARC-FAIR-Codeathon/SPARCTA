9251aec2fb381e6629c7250c42ae069c8f075e75
bhatnagararchit <bhatnagararchit@users.noreply.github.com>
2024-08-12 14:24:04 +0200

    Updated requirements.txt
    Tested on python==3.12

--- backend/flask_app/app/__init__.py
+++ backend/flask_app/app/__init__.py
@@ -1,95 +1,94 @@
-# from app import app
-from flask import send_file, request, render_template, Flask
-from PIL import Image
 import json
-from tifffile import imread
-
+from flask import request, render_template, Flask, abort, send_from_directory
 from app.models import Image, Annotation
+from app.images import create_test_dataset, get_dataset_tif_file
 
 app = Flask(__name__)
 
+# Download test dataset and convert tiff to dzi
+REMAKE_TEST_DATASET = False
+if REMAKE_TEST_DATASET:
+    create_test_dataset()
 
-@app.route('/')
+
+@app.route("/image/dzi", methods=["GET"])
 def get_image():
-    img = imread(
-        '/home/devil/Documents/AnmolKiran/SPARC/2024-team-6/backend/flask_app/app/app/test.tif'
-    )
-    Image.fromarray(img).save('test.png')
-    return send_file('test.png')
+    tif_path = request.args.get('tif')
+    dzi_file = get_dataset_tif_file(tif_path)
+    if dzi_file is None:
+        abort(404)
+    else:
+        return send_from_directory(dzi_file.parent, dzi_file.name)
 
-
-@app.route('/image_view')
+@app.route("/image_view")
 def image_view():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/get_annotations', methods=['POST', 'GET'])
+@app.route("/get_annotations", methods=["POST", "GET"])
 def get_annotations():
     data = request.form.to_dict()
-    imageid = data['imageid']
+    imageid = data["imageid"]
     annotations = Image.filter(Image.image_name == imageid).first().annotations
     annotation_list = []
 
     for annotation in annotations:
-        annotation_list.append({
-            'annotation':
-            annotation.annotation,
-            'annotation_coordinates':
-            annotation.annotation_coordinates
-        })
+        annotation_list.append(
+            {
+                "annotation": annotation.annotation,
+                "annotation_coordinates": annotation.annotation_coordinates,
+            }
+        )
 
     return json.dumps(annotation_list)
 
 
-@app.route('/add_annotations', methods=['POST'])
+@app.route("/add_annotations", methods=["POST"])
 def add_annotations():
     data = request.form.to_dict()
-    annotations = data['annotations']
-    imageid = data['imageid']
+    annotations = data["annotations"]
+    imageid = data["imageid"]
     images = Image.filter(Image.image_name == imageid).all()
     if not images:
-        return 'image not found'
+        return "image not found"
     elif len(images) > 1:
-        return 'multiple images found'
+        return "multiple images found"
 
     imges = images[0]
 
     for annotation in annotations:
-        ann = Annotation(annotation['annotation'],
-                         annotation['annotation_coordinates'])
-        imges['annotations'].append(ann)
+        ann = Annotation(annotation["annotation"], annotation["annotation_coordinates"])
+        imges["annotations"].append(ann)
     db_session.commit()
 
-    return 'success'
+    return "success"
 
 
-@app.route('/remove_annotations', methods=['POST'])
+@app.route("/remove_annotations", methods=["POST"])
 def remove_annotations():
     data = request.form.to_dict()
-    for annotation in data['annotations']:
+    for annotation in data["annotations"]:
         Annotation.filter(Annotation.annotation == annotation).delete()
 
     db_session.commit()
 
-    return 'success'
+    return "success"
 
 
-@app.route('/update_annotations', methods=['POST'])
+@app.route("/update_annotations", methods=["POST"])
 def update_annotations():
     data = request.form.to_dict()
 
-    for annotation in data['annotations']:
-        ann = Annotation.filter(
-            Annotation.annotation == annotation['annotation'])
-        ann.update(
-            {'annotation_coordinates': annotation['annotation_coordinates']})
+    for annotation in data["annotations"]:
+        ann = Annotation.filter(Annotation.annotation == annotation["annotation"])
+        ann.update({"annotation_coordinates": annotation["annotation_coordinates"]})
     db_session.commit()
 
-    return 'success'
+    return "success"
 
 
 # main driver function
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # run() method of Flask class runs the application
     # on the local development server.
