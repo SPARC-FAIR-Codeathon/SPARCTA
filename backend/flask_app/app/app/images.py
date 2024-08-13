f84eb7211ad08241dc7522a67d2d7fefb10f7cdd
bhatnagararchit <bhatnagararchit@users.noreply.github.com>
2024-08-13 04:57:46 +0200

    Updated docstrings

--- backend/flask_app/app/app/images.py
+++ backend/flask_app/app/app/images.py
@@ -13,6 +13,12 @@ import deepzoom
 
 
 class New_Dataset_Api(Dataset_Api):
+    """Dataset_Api modified to allow saving the dataset to any given path
+
+    Adds new attribute root_path, which is the path at which downloaded 
+    datasets is to be stored. Modifies _mkdir and _parse functions to ensure
+    that paths are pointed correctly.
+    """
     def __init__(self, root_path=""):
         self.root_path = Path(root_path)
         super().__init__()
@@ -43,6 +49,22 @@ class New_Dataset_Api(Dataset_Api):
 
 
 class TiffDziConvert:
+    """TIFF to DZI converter
+
+    Creates instances that can convert TIFF files into DZI files,
+    using deepZoom.ImageCreator.
+
+    All init arguments are passed to deepZoom.ImageCreator.
+
+    Any instance takes one argument, the path to the TIFF file, as 
+    input. TIFF file is rescaled to 8-bit depth before conversion to 
+    DZI, since deepZoom.ImageCreator only supports png or jpeg. If 
+    TIFF is 3 (4) channel, it is assumed to be RGB (RGBA).
+
+    DZI file (with the same name as the input TIFF file) and its tiles 
+    are stored in a new folder of the same name as the input TIFF file,
+    in the same folder as the TIFF file.
+    """
     def __init__(self, *args, **kwargs):
         self.creator = deepzoom.ImageCreator(*args, **kwargs)
 
@@ -70,12 +92,16 @@ class TiffDziConvert:
 
 
 TEST_DATASET_ID = 125
+"""ID for test dataset"""
 
 STATIC_PATH = Path(__file__).absolute().parent.parent / "static"
+"""Path to static folder"""
 
 STATIC_DATASET_PATH = STATIC_PATH / "dataset"
+"""Path where test dataset is stored when downloaded"""
 
 TEST_IMG_PATH = STATIC_PATH / "test_img/test_img.dzi"
+"""Path to test img, always present"""
 
 TIFF_DZI_CONVERTER = TiffDziConvert(
     tile_size=128,
@@ -84,9 +110,16 @@ TIFF_DZI_CONVERTER = TiffDziConvert(
     image_quality=1,
     resize_filter="bicubic",
 )
+"""Default TIFF to DZI converter"""
 
 
 def download_dataset(dataset_id):
+    """Downloads dataset from SPARC with id as dataset_id.
+    Dataset is downloaded at STATIC_DATASET_PATH. If the 
+    path already exists, it is overwritten.
+
+    :param dataset_id: ID of SPARC dataset to download
+    """
     if STATIC_DATASET_PATH.exists():
         rmtree(STATIC_DATASET_PATH)
     STATIC_DATASET_PATH.mkdir()
@@ -95,6 +128,11 @@ def download_dataset(dataset_id):
 
 
 def convert_tif_dzi(converter: TiffDziConvert):
+    """Converts all TIFFs at dataset at STATIC_DATASET_PATH into 
+    DZI, using given converter.
+
+    :param converter: TIFF to DZI converter
+    """
     tif_files = itertools.chain(
         STATIC_DATASET_PATH.rglob("*.tif"), STATIC_DATASET_PATH.rglob("*.tiff")
     )
@@ -103,11 +141,21 @@ def convert_tif_dzi(converter: TiffDziConvert):
 
 
 def create_test_dataset():
+    """Downloads test dataset (TEST_DATASET_ID) and converts all
+    TIFF files in the dataset into DZI using TIFF_DZI_CONVERTER
+    """
     download_dataset(TEST_DATASET_ID)
     convert_tif_dzi(TIFF_DZI_CONVERTER)
 
 
 def _get_dataset_tif_file(relative_path):
+    """Returns absolute path to DZI file, given relative path from url
+    to corresponding TIFF file
+
+    :param relative_path: url relative path to TIFF file
+    :return: If DZI file exists, returns the path to it. If it doesn't, 
+    returns None.
+    """
     if relative_path  == "test":
         return TEST_IMG_PATH
     if relative_path[0] == "/":
@@ -119,6 +167,13 @@ def _get_dataset_tif_file(relative_path):
         return dzi_file
     
 def get_dataset_tif_file(relative_path):
+    """Returns url path to DZI file relative to STATIC_PATH.parent, given 
+    relative path from url to corresponding TIFF file
+
+    :param relative_path: url relative path to TIFF file
+    :return: If DZI file exists, returns the url relative path to it. If 
+    it doesn't, returns None.
+    """
     dzi_file = _get_dataset_tif_file(relative_path)
     if dzi_file is not None:        
         # Relative to static
